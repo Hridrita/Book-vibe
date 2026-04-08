e7db8d29a587ac339bd329b82c72e9e9fbf2f3f0
Hridrita <moonsaha721@gmail.com>
2026-04-08 16:58:18 +0600

    completed banner

--- src/Pages/Homepage/Banner.jsx
+++ src/Pages/Homepage/Banner.jsx
@@ -1,22 +1,18 @@
 import React from "react";
+import BookImg from "../../assets/pngwing-1.png"
 
 const Banner = () => {
   return (
     <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+      <div className="hero bg-base-200 min-h-[70vh] rounded-2xl my-8 container mx-auto">
+        <div className="hero-content flex-col lg:flex-row-reverse w-full justify-between">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            src={BookImg}
             className="max-w-sm rounded-lg shadow-2xl"
           />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+            <h1 className="text-5xl font-bold">Books to freshen up <br></br>your bookshelf</h1>
+            <button className="btn btn-success mt-6">View the List</button>
           </div>
         </div>
       </div>
