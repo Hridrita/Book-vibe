a1acbd300635b38c003c7c4246e756f6b9385c2c
Hridrita <moonsaha721@gmail.com>
2026-04-09 23:56:24 +0600

    added sort functionaltiy in wishlist

--- src/Components/ListedBooks/ListWishList.jsx
+++ src/Components/ListedBooks/ListWishList.jsx
@@ -1,12 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { BookContext } from '../../Context/BookContext';
 import BookCard from '../../Pages/Homepage/BookCard';
 
-const ListWishList = () => {
+const ListWishList = ({sortingType}) => {
      const { wishList } = useContext(BookContext);
                   console.log(wishList);
 
-                  if(wishList.length === 0){
+     const [filteredWishList, setFilteredWishList] = useState(wishList);
+     
+     
+           useEffect(()=> {
+             if(sortingType){
+                 if(sortingType === 'pages'){
+                     const sortedData = [...wishList].sort((a,b)=> a.totalPages - b.totalPages);
+                     console.log(sortedData);
+                     setFilteredWishList(sortedData);
+                 }else if(sortingType === 'rating'){
+                     const sortedData = [...wishList].sort((a,b)=> a.rating - b.rating);
+                     console.log(sortedData);
+                     setFilteredWishList(sortedData);
+                 }
+             }
+     
+           }, [sortingType, wishList]);
+
+                  if(filteredWishList.length === 0){
                     return <div className='h-[50vh] bg-gray-100 flex items-center justify-center'>
                         <h2 className='font-bold text-3xl'>No Wish List Data Found</h2>
                     </div>
@@ -15,7 +33,7 @@ const ListWishList = () => {
         <div>
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
                         {
-                            wishList.map((book, index) => <BookCard key={index} book={book}></BookCard>)
+                            filteredWishList.map((book, index) => <BookCard key={index} book={book}></BookCard>)
                         }
                     </div>
         </div>
